96c01e2641a849be969ee7717d9bc3731efa7d98
Antonio Miguel Pozo Cámara <apozoetsiit@correo.ugr.es>
2016-01-19 20:04:07 +0100

    tocando settings

--- proyectoP4/settings.py
+++ proyectoP4/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango',
+    'Bares',
 	'easy_maps',
 	'registration', # add in the registration package
 )
@@ -54,7 +54,7 @@ INSTALLED_APPS = (
 REGISTRATION_OPEN = True                # If True, users can register
 ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
-LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
+LOGIN_REDIRECT_URL = '/Bares/'  # The page you want users to arrive at after they successful log in
 LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
                                                                 # and are trying to access pages requiring authentication
 
