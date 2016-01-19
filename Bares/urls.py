1f36f4b6f4bec580821887de1ca79705e3385dc0
Antonio Miguel Pozo Cámara <apozoetsiit@correo.ugr.es>
2016-01-19 11:09:12 +0100

    tocando el settings

--- Bares/urls.py
+++ Bares/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from Bares import views
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
