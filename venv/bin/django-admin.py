6df5d5d55a454e877b933b9e1a603446912bde34
Antonio Miguel Pozo Cámara <apozoetsiit@correo.ugr.es>
2016-01-21 09:56:16 +0100

    subiendo .gitignore

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Apozo/Dropbox/1C/DAI/practicas/barchecker-heroku/proyectoP4/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
