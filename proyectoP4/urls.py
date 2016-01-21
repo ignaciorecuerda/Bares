d997bb42635c62e5c05b5cfc6017cfff9ea46db6
Antonio Miguel Pozo Cámara <apozoetsiit@correo.ugr.es>
2016-01-21 09:26:23 +0100

    copiando de nuevo mi app

--- proyectoP4/urls.py
+++ proyectoP4/urls.py
@@ -1,37 +1,44 @@
-from django.conf.urls import patterns, include, url
+# -*- coding: utf-8 -*-
+"""proyectoP4 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
-from django.conf import settings # New Import
-from django.conf.urls.static import static # New Import
-from registration.backends.simple.views import RegistrationView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from Bares import views
+from django.conf import settings
 
-# Create a new class that redirects the user to the index page, if successful at logging
-class MyRegistrationView(RegistrationView):
-	def get_success_url(self,request, user):
-		return '/rango/'
-
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'tango_with_django_project_17.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	
-	url(r'^admin/', include(admin.site.urls)),
-	url(r'^Bares/', include('Bares.urls')),
-	url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
-	url(r'^accounts/', include('registration.backends.simple.urls')),
-)
 
-if not settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^Bares/', include('Bares.urls')), # ADD THIS NEW TUPLE!media/(?P<path>.*)
+]
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
 
 if settings.DEBUG:
-	urlpatterns += patterns(
-		'django.views.static',
-		(r'^media/(?P<path>.*)',
-		'serve',
-		{'document_root': settings.MEDIA_ROOT}), )
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
+else:
+    urlpatterns += patterns('', url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_PATH}),
+    )
+
+
+# urlpatterns += patterns('django.views.static', (r'^media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}), )
+#urlpatterns += patterns('django.views.static', (r'^media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}), )
